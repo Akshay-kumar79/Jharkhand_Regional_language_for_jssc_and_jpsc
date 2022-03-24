c0e4137a408a2ec05e1e87004c60fb9302ab33db
Akshay kumar shaw <akshay.8459361606@gmail.com>
2022-03-24 21:34:04 +0530

    n

--- app/src/main/java/ashutosh/jharkhand/regional/MainActivity.kt
+++ app/src/main/java/ashutosh/jharkhand/regional/MainActivity.kt
@@ -2,10 +2,14 @@ package ashutosh.jharkhand.regional
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import ashutosh.jharkhand.regional.databinding.ActivityMainBinding
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +24,14 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
+        MobileAds.initialize(this)
+        setUpBannerAd()
+
         setUpToolBar()
+    }
+
+    private fun setUpBannerAd() {
+        binding.adView.loadAd(AdRequest.Builder().build())
     }
 
     private fun setUpToolBar() {
--- app/src/main/java/ashutosh/jharkhand/regional/screens/QuizFragment.kt
+++ app/src/main/java/ashutosh/jharkhand/regional/screens/QuizFragment.kt
@@ -1,22 +1,30 @@
 package ashutosh.jharkhand.regional.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import ashutosh.jharkhand.regional.R
 import ashutosh.jharkhand.regional.databinding.FragmentQuizBinding
 import ashutosh.jharkhand.regional.viewModels.QuizViewModel
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class QuizFragment : Fragment() {
 
     private val args: QuizFragmentArgs by navArgs()
     private lateinit var binding: FragmentQuizBinding
     private lateinit var quizViewModel: QuizViewModel
+    private var mInterstitialAd: InterstitialAd? = null
 
 
     override fun onCreateView(
@@ -31,6 +39,7 @@ class QuizFragment : Fragment() {
         binding.quizViewModel = quizViewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
+        readyInterstitialAd()
         setObservers()
 
         return binding.root
@@ -38,18 +47,64 @@ class QuizFragment : Fragment() {
 
     private fun setObservers() {
 
-        quizViewModel.showScore.observe(viewLifecycleOwner){
-            if (it){
-                val dialog =ScoreFragmentDialog()
+        quizViewModel.showScore.observe(viewLifecycleOwner) {
+            if (it) {
+                showInterstitialAd()
+
+                val dialog = ScoreFragmentDialog()
                 dialog.show(requireActivity().supportFragmentManager, dialog.tag)
                 quizViewModel.showScoreDone()
             }
         }
 
-        quizViewModel.isQuizDone.observe(viewLifecycleOwner){
-            if (it){
+        quizViewModel.isQuizDone.observe(viewLifecycleOwner) {
+            if (it) {
                 findNavController().popBackStack()
             }
+        }
+    }
+
+    private fun readyInterstitialAd(){
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(
+            requireContext(),
+            getString(R.string.interstitial_ad_unit_id),
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    mInterstitialAd = null
+                }
+
+                override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                    mInterstitialAd = interstitialAd
+
+                    mInterstitialAd?.fullScreenContentCallback = object: FullScreenContentCallback() {
+                        override fun onAdDismissedFullScreenContent() {
+                            Log.d("TAG", "Ad was dismissed.")
+                        }
+
+                        override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                            Log.d("TAG", "Ad failed to show.")
+                        }
+
+                        override fun onAdShowedFullScreenContent() {
+                            Log.d("TAG", "Ad showed fullscreen content.")
+                            mInterstitialAd = null
+                        }
+                    }
+                }
+            }
+        )
+
+    }
+
+    private fun showInterstitialAd(){
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(requireActivity())
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.")
         }
     }
 
